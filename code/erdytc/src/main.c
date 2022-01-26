435c2f9c885d2df3424f5b2ce94e89d61dc2f034
AugustoRiedinger <riedinger.augusto@gmail.com>
2022-01-26 19:31:41 -0300

    [C-E] Changed code structure in .org file

--- code/erdytc/src/main.c
+++ code/erdytc/src/main.c
@@ -1,25 +1,41 @@
+/*----------------------------------------------------------------*/
+/*LIBRERIAS:                                                      */
+/*----------------------------------------------------------------*/
+/*Librerias estandar:*/
 #include "stm32f4xx.h"
 #include "stm32f4xx_gpio.h"
 #include "stdio.h"
 #include "misc.h"
 
+/*Control del clock:*/
 #include "stm32f4xx_rcc.h"
 
+/*Control de timers:*/
 #include "stm32f4xx_tim.h"
 
+/*Matematicas:*/
 #include "math.h"
 
-GPIO_InitTypeDef    GPIO_InitStructure;
+/*----------------------------------------------------------------*/
+/*DEFINICIONES:                                                   */
+/*----------------------------------------------------------------*/
 
-NVIC_InitTypeDef    NVIC_InitStructure;
+/* * * * * * * * * * * * * ESTRUCTURAS * * * * * * * * * * * * */
+/*General:*/
+GPIO_InitTypeDef        GPIO_InitStructure;
+NVIC_InitTypeDef        NVIC_InitStructure;
+EXTI_InitTypeDef        EXTI_InitStructure;
 
-EXTI_InitTypeDef   EXTI_InitStructure;
-
+/*TIMERS*/
 TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
 TIM_OCInitTypeDef       TIM_OCInitStructure;
 
-DMA_InitTypeDef     DMA_InitStructure;
+/*DMA*/
+DMA_InitTypeDef         DMA_InitStructure;
 
+/* * * * * * * * * * * * * CONSTANTES * * * * * * * * * * * * */
+/* - - - -   LCD   - - - -*/
+/*Constantes varias:*/
 #define  TLCD_INIT_PAUSE        100000
 #define  TLCD_PAUSE             50000
 #define  TLCD_CLK_PAUSE         1000
@@ -37,6 +53,7 @@ DMA_InitTypeDef     DMA_InitStructure;
 #define  TLCD_CMD_DISP_M3       0x0F
 #define  TLCD_CMD_CLEAR         0x01
 
+/*Identificacion pines:*/
 typedef enum
 {
   TLCD_RS = 0,  // RS-Pin
@@ -47,8 +64,10 @@ typedef enum
   TLCD_D7 = 5   // DB7-Pin
 }TLCD_NAME_t;
 
+/*Nombre LCD:*/
 #define  TLCD_ANZ   6
 
+/*Estados LCD:*/
 typedef enum {
   TLCD_OFF = 0,
   TLCD_ON,
@@ -56,6 +75,7 @@ typedef enum {
   TLCD_BLINK
 }TLCD_MODE_t;
 
+/*Crear LCD:*/
 typedef struct {
   TLCD_NAME_t TLCD_NAME;
   GPIO_TypeDef* TLCD_PORT;
@@ -64,8 +84,12 @@ typedef struct {
   BitAction TLCD_INIT;
 }LCD_2X16_t;
 
-uint32_t FIND_CLOCK(GPIO_TypeDef* Port);
+/* - - - - HARDWARE - - - -*/
 
+/* - - - - PARAMS. - - - -*/
+
+/* * * * * * * * * * * * * FUNCIONES * * * * * * * * * * * * */
+/*Parametros LCD:*/
 void P_LCD_2x16_InitIO(LCD_2X16_t* LCD_2X16);
 void P_LCD_2x16_PinLo(TLCD_NAME_t lcd_pin, LCD_2X16_t* LCD_2X16);
 void P_LCD_2x16_PinHi(TLCD_NAME_t lcd_pin, LCD_2X16_t* LCD_2X16);
@@ -76,23 +100,42 @@ void P_LCD_2x16_Cmd(uint8_t wert, LCD_2X16_t* LCD_2X16);
 void P_LCD_2x16_Cursor(LCD_2X16_t* LCD_2X16, uint8_t x, uint8_t y);
 void P_LCD_2x16_Data(uint8_t wert, LCD_2X16_t* LCD_2X16);
 
+/*Inicializacion LCD:*/
 void INIT_LCD_2x16(LCD_2X16_t* LCD_2X16);
 
+/*Refrescar LCD:*/
 void CLEAR_LCD_2x16(LCD_2X16_t* LCD_2X16);
 
+/*Imprimir LCD:*/
 void PRINT_LCD_2x16(LCD_2X16_t* LCD_2X16, uint8_t x, uint8_t y, char *ptr);
 
+/*Inicializacion TIM3:*/
 void INIT_TIM3(uint32_t freq);
 
+/*Encontrar CLOCK:*/
+uint32_t FIND_CLOCK(GPIO_TypeDef* Port);
+
+/*----------------------------------------------------------------*/
+/*MAIN:                                                           */
+/*----------------------------------------------------------------*/
 int main(void){
 
+/* * * * * * * * * * * * * INICIALIZ. * * * * * * * * * * * * */
+    /*Inicio del sistema:*/
     SystemInit();
 
+/* * * * * * * * * * * * * BUCLE PPAL. * * * * * * * * * * * * */
   while (1)
   {
   }
 }
 
+/*----------------------------------------------------------------*/
+/*FUNCIONES LOCALES:                                              */
+/*----------------------------------------------------------------*/
+
+/* * * * * * * * * * * * *     LCD    * * * * * * * * * * * * */
+/*Inicializacion pines LCD:*/
 void P_LCD_2x16_InitIO(LCD_2X16_t* LCD_2X16)
 {
     GPIO_InitTypeDef GPIO_InitStructure;
@@ -119,6 +162,7 @@ void P_LCD_2x16_InitIO(LCD_2X16_t* LCD_2X16)
     }
 }
 
+/*Cambio de estados del LCD:*/
 void P_LCD_2x16_PinLo(TLCD_NAME_t lcd_pin, LCD_2X16_t* LCD_2X16)
 {
   LCD_2X16[lcd_pin].TLCD_PORT->BSRRH = LCD_2X16[lcd_pin].TLCD_PIN;
@@ -129,6 +173,7 @@ void P_LCD_2x16_PinHi(TLCD_NAME_t lcd_pin, LCD_2X16_t* LCD_2X16)
   LCD_2X16[lcd_pin].TLCD_PORT->BSRRL = LCD_2X16[lcd_pin].TLCD_PIN;
 }
 
+/*Retardo LCD:*/
 void P_LCD_2x16_Delay(volatile uint32_t nCount)
 {
   while(nCount--)
@@ -136,6 +181,7 @@ void P_LCD_2x16_Delay(volatile uint32_t nCount)
   }
 }
 
+/*Inicializacion secuencia LCD:*/
 void P_LCD_2x16_InitSequenz(LCD_2X16_t* LCD_2X16)
 {
   //Inicializacion de la secuencia:
@@ -161,6 +207,7 @@ void P_LCD_2x16_InitSequenz(LCD_2X16_t* LCD_2X16)
   P_LCD_2x16_Delay(TLCD_PAUSE);
 }
 
+/*Configuracion CLOCK LCD:*/
 void P_LCD_2x16_Clk(LCD_2X16_t* LCD_2X16)
 {
   // Pin-E auf Hi
@@ -173,6 +220,7 @@ void P_LCD_2x16_Clk(LCD_2X16_t* LCD_2X16)
   P_LCD_2x16_Delay(TLCD_CLK_PAUSE);
 }
 
+/*Comandos LCD:*/
 void P_LCD_2x16_Cmd(uint8_t wert, LCD_2X16_t* LCD_2X16)
 {
   // RS=Lo (Command)
@@ -204,6 +252,7 @@ void P_LCD_2x16_Cursor(LCD_2X16_t* LCD_2X16, uint8_t x, uint8_t y)
   P_LCD_2x16_Cmd(wert,LCD_2X16);
 }
 
+/*Analisis de datos LCD:*/
 void P_LCD_2x16_Data(uint8_t wert, LCD_2X16_t* LCD_2X16)
 {
   // RS=Hi (Data)
@@ -222,6 +271,7 @@ void P_LCD_2x16_Data(uint8_t wert, LCD_2X16_t* LCD_2X16)
   P_LCD_2x16_Clk(LCD_2X16);
 }
 
+/*Inicializacion de los pines del LCD:*/
 void INIT_LCD_2x16(LCD_2X16_t* LCD_2X16)
 {
     //Inicialización de los pines del LCD:
@@ -241,6 +291,7 @@ void INIT_LCD_2x16(LCD_2X16_t* LCD_2X16)
     P_LCD_2x16_Delay(TLCD_PAUSE);
 }
 
+/*Refresco de la pantalla del LCD:*/
 void CLEAR_LCD_2x16(LCD_2X16_t* LCD_2X16)
 {
   // Display l�schen
@@ -249,6 +300,7 @@ void CLEAR_LCD_2x16(LCD_2X16_t* LCD_2X16)
   P_LCD_2x16_Delay(TLCD_PAUSE);
 }
 
+/*Impresion en la pantalla del LCD:*/
 void PRINT_LCD_2x16(LCD_2X16_t* LCD_2X16, uint8_t x, uint8_t y, char *ptr)
 {
   // Cursor setzen
