58be81e29fcf5c3edc19fd46a3a5b8d26b84f982
AugustoRiedinger <riedinger.augusto@gmail.com>
2022-02-06 11:46:25 -0300

    [C-S] Finished ADC configuration and subrutines - Finished USART subrutines

--- code/satdac/src/main.c
+++ code/satdac/src/main.c
@@ -47,6 +47,10 @@ DMA_InitTypeDef         DMA_InitStructure;
 #define _servo      GPIO_Pin_3
 #define __servo     GPIO_PinSource3
 
+/*Sensor temperatura:*/
+#define _LM35   GPIOC
+#define _lm35   GPIO_Pin_0
+
 /* - - - - PARAMS. - - - -*/
 /*Frecuencia PWM:*/
 #define freqPWM 10e3
@@ -70,6 +74,18 @@ uint32_t receivedCode = 0;
 uint32_t temp = 0;
 
 /* * * * * * * * * * * * * FUNCIONES * * * * * * * * * * * * */
+/*Inicializacion ADC:*/
+void INIT_ADC(GPIO_TypeDef* Port, uint16_t Pin);
+
+/*Funciones configuracion ADC:*/
+ADC_TypeDef* FIND_ADC_TYPE(GPIO_TypeDef* Port, uint32_t Pin);
+uint32_t     FIND_RCC_APB (ADC_TypeDef*  ADCX);
+uint8_t      FIND_CHANNEL (GPIO_TypeDef* Port, uint32_t Pin);
+uint32_t     FIND_CLOCK   (GPIO_TypeDef* Port);
+
+/*Leer ADC:*/
+int32_t READ_ADC(GPIO_TypeDef* Port, uint16_t Pin);
+
 /*Inicializacion TIM3:*/
 void INIT_TIM3(uint32_t freq);
 
@@ -99,6 +115,9 @@ int main(void){
 /* * * * * * * * * * * * * INICIALIZ. * * * * * * * * * * * * */
     /*Inicio del sistema:*/
     SystemInit();
+
+    /*Inicializacion ADC:*/
+    INIT_ADC(_LM35, _lm35);
 
     /*Inicializacion de timers:*/
     INIT_TIMPWM();
@@ -134,6 +153,204 @@ void START_TEMP(void)
 
     /*Enviar el dato de temperatura leido:*/
     SEND_TEMP();
+}
+
+/*Enviar valor de temperatura:*/
+void SEND_TEMP()
+{
+    /*Clarear el flag de estado para transmitir:*/
+    while (USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET)
+    {}
+
+    /*Iniciar la transmision:*/
+    USART_SendData(USART2, temp);
+}
+
+void INIT_ADC(GPIO_TypeDef* Port, uint16_t Pin)
+{
+    uint32_t Clock;
+    Clock = FIND_CLOCK(Port);
+
+    ADC_TypeDef* ADCX;
+    ADCX = FIND_ADC_TYPE(Port, Pin);
+
+    uint32_t RCC_APB;
+    RCC_APB = FIND_RCC_APB(ADCX);
+
+    uint8_t Channel;
+    Channel = FIND_CHANNEL(Port, Pin);
+
+    GPIO_InitTypeDef        GPIO_InitStructure;
+    ADC_InitTypeDef         ADC_InitStructure;
+    ADC_CommonInitTypeDef   ADC_CommonInitStructure;
+
+    /*Habilitacion Clock en el puerto donde esta conectado el ADC:*/
+    RCC_AHB1PeriphClockCmd(Clock, ENABLE);
+
+    /*Configuracion del PIN del ADC como entrada ANALOGICA.*/
+    GPIO_StructInit(&GPIO_InitStructure);
+    GPIO_InitStructure.GPIO_Pin     = Pin;
+    GPIO_InitStructure.GPIO_Mode    = GPIO_Mode_AN;
+    GPIO_InitStructure.GPIO_PuPd    = GPIO_PuPd_NOPULL ;
+    GPIO_Init(Port, &GPIO_InitStructure);
+
+    /*Activar ADC:*/
+    RCC_APB2PeriphClockCmd(RCC_APB, ENABLE);
+
+    /*ADC Common Init:*/
+    ADC_CommonStructInit(&ADC_CommonInitStructure);
+    ADC_CommonInitStructure.ADC_Mode = ADC_Mode_Independent;
+    ADC_CommonInitStructure.ADC_Prescaler = ADC_Prescaler_Div4;
+    ADC_CommonInitStructure.ADC_DMAAccessMode = ADC_DMAAccessMode_Disabled;
+    ADC_CommonInitStructure.ADC_TwoSamplingDelay = ADC_TwoSamplingDelay_5Cycles;
+    ADC_CommonInit(&ADC_CommonInitStructure);
+
+    /*ADC Init:*/
+    ADC_StructInit (&ADC_InitStructure);
+    ADC_InitStructure.ADC_Resolution             = ADC_Resolution_12b;
+    ADC_InitStructure.ADC_ScanConvMode           = DISABLE;
+    ADC_InitStructure.ADC_ContinuousConvMode     = DISABLE;
+    ADC_InitStructure.ADC_ExternalTrigConvEdge   = ADC_ExternalTrigConvEdge_None;
+    ADC_InitStructure.ADC_DataAlign              = ADC_DataAlign_Right;
+    ADC_InitStructure.ADC_NbrOfConversion        = 1;
+    ADC_Init(ADCX, &ADC_InitStructure);
+
+    /*Establecer la configuración de conversion:*/
+    ADC_InjectedSequencerLengthConfig(ADCX, 1);
+    ADC_SetInjectedOffset(ADCX, ADC_InjectedChannel_1, 0);
+    ADC_InjectedChannelConfig(ADCX, Channel, 1, ADC_SampleTime_480Cycles);
+
+    /*Poner en marcha ADC:*/
+    ADC_Cmd(ADCX, ENABLE);
+}
+
+int32_t READ_ADC(GPIO_TypeDef* Port, uint16_t Pin)
+{
+    uint32_t ADC_DATA;
+
+    ADC_TypeDef* ADCX;
+    ADCX = FIND_ADC_TYPE(Port, Pin);
+
+    ADC_ClearFlag(ADCX, ADC_FLAG_JEOC);
+    ADC_SoftwareStartInjectedConv(ADCX);
+    while (ADC_GetFlagStatus(ADCX, ADC_FLAG_JEOC) == RESET);
+
+    ADC_DATA = ADC_GetInjectedConversionValue(ADCX, ADC_InjectedChannel_1);
+    return ADC_DATA;
+}
+
+uint32_t FIND_CLOCK(GPIO_TypeDef* Port)
+{
+    uint32_t Clock;
+
+    if      (Port == GPIOA) Clock = RCC_AHB1Periph_GPIOA;
+    else if (Port == GPIOB) Clock = RCC_AHB1Periph_GPIOB;
+    else if (Port == GPIOC) Clock = RCC_AHB1Periph_GPIOC;
+    else if (Port == GPIOD) Clock = RCC_AHB1Periph_GPIOD;
+    else if (Port == GPIOE) Clock = RCC_AHB1Periph_GPIOE;
+    else if (Port == GPIOF) Clock = RCC_AHB1Periph_GPIOF;
+    else if (Port == GPIOG) Clock = RCC_AHB1Periph_GPIOG;
+    return Clock;
+}
+
+ADC_TypeDef* FIND_ADC_TYPE (GPIO_TypeDef* Port, uint32_t Pin)
+{
+    ADC_TypeDef* ADCX;
+
+    if ((Port == GPIOA && (Pin == GPIO_Pin_0 || Pin == GPIO_Pin_1   ||
+                           Pin == GPIO_Pin_2 || Pin == GPIO_Pin_3   ||
+                           Pin == GPIO_Pin_4 || Pin == GPIO_Pin_5   ||
+                           Pin == GPIO_Pin_6 || Pin == GPIO_Pin_7)) ||
+
+        (Port == GPIOB && (Pin == GPIO_Pin_0 || Pin == GPIO_Pin_1)) ||
+        (Port == GPIOC && (Pin == GPIO_Pin_0 || Pin == GPIO_Pin_1   ||
+                           Pin == GPIO_Pin_2 || Pin == GPIO_Pin_3   ||
+                           Pin == GPIO_Pin_4 || Pin == GPIO_Pin_5)))
+        ADCX = ADC1;
+
+    else if ((Port == GPIOF && (Pin == GPIO_Pin_3 ||
+                                Pin == GPIO_Pin_4 ||
+                                Pin == GPIO_Pin_5 ||
+                                Pin == GPIO_Pin_6 ||
+                                Pin == GPIO_Pin_7 ||
+                                Pin == GPIO_Pin_8 ||
+                                Pin == GPIO_Pin_9 ||
+                                Pin == GPIO_Pin_10)))
+        ADCX = ADC3;
+
+    else
+        ADCX = NULL;
+
+    return ADCX;
+}
+
+uint32_t FIND_RCC_APB(ADC_TypeDef* ADCX)
+{
+    uint32_t RCC_APB;
+
+    if      (ADCX == ADC1) RCC_APB = RCC_APB2Periph_ADC1;
+    else if (ADCX == ADC3) RCC_APB = RCC_APB2Periph_ADC3;
+    else     RCC_APB = 0;
+
+    return RCC_APB;
+}
+
+uint8_t FIND_CHANNEL(GPIO_TypeDef* Port, uint32_t Pin)
+{
+    uint8_t Channel;
+
+    if      (Port == GPIOA && Pin == GPIO_Pin_0)
+        Channel = ADC_Channel_0;
+    else if (Port == GPIOA && Pin == GPIO_Pin_1)
+        Channel = ADC_Channel_1;
+    else if (Port == GPIOA && Pin == GPIO_Pin_2)
+        Channel = ADC_Channel_2;
+    else if (Port == GPIOA && Pin == GPIO_Pin_3)
+        Channel = ADC_Channel_3;
+    else if (Port == GPIOA && Pin == GPIO_Pin_4)
+        Channel = ADC_Channel_4;
+    else if (Port == GPIOA && Pin == GPIO_Pin_5)
+        Channel = ADC_Channel_5;
+    else if (Port == GPIOA && Pin == GPIO_Pin_6)
+        Channel = ADC_Channel_6;
+    else if (Port == GPIOA && Pin == GPIO_Pin_7)
+        Channel = ADC_Channel_7;
+    else if (Port == GPIOB && Pin == GPIO_Pin_0)
+        Channel = ADC_Channel_8;
+    else if (Port == GPIOB && Pin == GPIO_Pin_1)
+        Channel = ADC_Channel_9;
+    else if (Port == GPIOC && Pin == GPIO_Pin_0)
+        Channel = ADC_Channel_10;
+    else if (Port == GPIOC && Pin == GPIO_Pin_1)
+        Channel = ADC_Channel_11;
+    else if (Port == GPIOC && Pin == GPIO_Pin_2)
+        Channel = ADC_Channel_12;
+    else if (Port == GPIOC && Pin == GPIO_Pin_3)
+        Channel = ADC_Channel_13;
+    else if (Port == GPIOC && Pin == GPIO_Pin_4)
+        Channel = ADC_Channel_14;
+    else if (Port == GPIOC && Pin == GPIO_Pin_5)
+        Channel = ADC_Channel_15;
+    else if (Port == GPIOF && Pin == GPIO_Pin_3)
+        Channel = ADC_Channel_9;
+    else if (Port == GPIOF && Pin == GPIO_Pin_4)
+        Channel = ADC_Channel_14;
+    else if (Port == GPIOF && Pin == GPIO_Pin_5)
+        Channel = ADC_Channel_15;
+    else if (Port == GPIOF && Pin == GPIO_Pin_6)
+        Channel = ADC_Channel_4;
+    else if (Port == GPIOF && Pin == GPIO_Pin_7)
+        Channel = ADC_Channel_5;
+    else if (Port == GPIOF && Pin == GPIO_Pin_8)
+        Channel = ADC_Channel_6;
+    else if (Port == GPIOF && Pin == GPIO_Pin_9)
+        Channel = ADC_Channel_7;
+    else if (Port == GPIOF && Pin == GPIO_Pin_10)
+        Channel = ADC_Channel_8;
+    else
+        Channel = 0;
+
+    return Channel;
 }
 
 void INIT_TIMPWM(void){
@@ -189,4 +406,9 @@ void INIT_SERVO(void){
     GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
     GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
     GPIO_Init(GPIOD, &GPIO_InitStruct);
+}
+
+void MOVE_SERVO()
+{
+
 }
